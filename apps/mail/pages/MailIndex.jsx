3d302274c5f8b14309fdb2d18fc43e70626721fc
alonWohl <alonw1010@gmail.com>
2024-10-03 12:34:51 +0300

    Add isStarred functionality

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -4,7 +4,8 @@ import { mailSevice } from '../services/mail.service.js'
 
 const { useState, useEffect } = React
 export function MailIndex() {
-  const [mails, setMails] = useState(null)
+  const [mails, setMails] = useState([])
+  // const [isStarred, setIsStarred] = useState(null)
 
   useEffect(() => {
     mailSevice
@@ -15,11 +16,25 @@ export function MailIndex() {
       })
   }, [])
 
+  function onToggleStarred(mailId) {
+    mailSevice.get(mailId).then((mail) => {
+      const updatedMail = { ...mail, isStarred: !mail.isStarred }
+      mailSevice
+        .save(updatedMail)
+        .then(() => {
+          setMails((prevMails) => [...prevMails.map((m) => (m.id === mailId ? updatedMail : m))].sort((a, b) => b.isStarred - a.isStarred))
+        })
+        .catch((err) => {
+          console.error('Failed to update mail:', err)
+        })
+    })
+  }
+
   if (!mails) return <AppLoader />
 
   return (
     <section className='mail-index'>
-      <MailList mails={mails} />
+      <MailList mails={mails} onToggleStarred={onToggleStarred} />
     </section>
   )
 }
