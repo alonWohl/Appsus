79239f132871db459a1d7c85345cce0bbc704348
alonWohl <alonw1010@gmail.com>
2024-10-09 02:04:41 +0300

    Css Changes

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -2,10 +2,12 @@ import { MailPreview } from './MailPreview.jsx'
 
 export function MailList({ mails, onToggleStarred, onRemoveMail, onToggleRead }) {
   return (
-    <ul className='mail-list'>
-      {mails.map((mail) => (
-        <MailPreview mail={mail} key={mail.id} onToggleStarred={onToggleStarred} onRemoveMail={onRemoveMail} onToggleRead={onToggleRead} />
-      ))}
-    </ul>
+    <section className='mail-list-container'>
+      <ul className='mail-list'>
+        {mails.map((mail) => (
+          <MailPreview mail={mail} key={mail.id} onToggleStarred={onToggleStarred} onRemoveMail={onRemoveMail} onToggleRead={onToggleRead} />
+        ))}
+      </ul>
+    </section>
   )
 }
