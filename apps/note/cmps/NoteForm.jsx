94343843320a14629ac6f58ef0768541fc9f7abb
AvivBuzaglo <avivbuzaglo99@gmail.com>
2024-10-11 20:26:50 +0300

    Add some css (not finish)

--- apps/note/cmps/NoteForm.jsx
+++ apps/note/cmps/NoteForm.jsx
@@ -3,10 +3,10 @@ export function NoteForm({onFormClick}) {
     
     return (
         <form className='note-form'>
-        <input type="text" placeholder='Take a note...' onClick={() => onFormClick('text')}/>
-        <button className='new-list-note-btn' onClick={() => onFormClick('list')}>List</button>
-        <button className='new-canvas-note-btn' onClick={() => onFormClick('draw')}>Drawing</button>
-        <button className='new-img-note-btn' onClick={() => onFormClick('image')}>Image</button>
+        <input type="text" placeholder='Take a note...' className='note-text-input' onClick={() => onFormClick('text')}/>
+        <button className='note-form-btn' onClick={() => onFormClick('list')}>List</button>
+        <button className='note-form-btn' onClick={() => onFormClick('draw')}>Drawing</button>
+        <button className='note-form-btn' onClick={() => onFormClick('image')}>Image</button>
     </form>
     )
 }
