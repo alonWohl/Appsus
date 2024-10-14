80676cd2983b6b7399ae31df0e7d95f1fdeb2870
AvivBuzaglo <avivbuzaglo99@gmail.com>
2024-10-14 15:19:02 +0300

    Add fixed pinned notes, Change NoteList layout

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -3,23 +3,27 @@ import { NotePreview } from "./NotePreview.jsx"
 const { useState, useEffect } = React
 
 export function NoteList({ notes, onRemoveNote, onArchiveNote, onNoteClick, onPinNote, pinnedNotes}) {
-
+    
     
     
     return (
         <React.Fragment>
             {/* <ul className='note-list'> */}
-                {pinnedNotes.length > 0 && <div className='note-list'>
-                    pinned
-                    {pinnedNotes.map((note) => (
-                    <NotePreview note={note} key={note.id} onRemoveNote={onRemoveNote} onArchiveNote={onArchiveNote} onNoteClick={onNoteClick} onPinNote={onPinNote} className='note'/>
-                    ))}
-                    <br />
-                    {notes.map((note) => (
-                    !note.isPinned && 
-                    <NotePreview note={note} key={note.id} onRemoveNote={onRemoveNote} onArchiveNote={onArchiveNote} onNoteClick={onNoteClick} onPinNote={onPinNote} className='note'/>
-                    ))}
-                </div> 
+                {pinnedNotes.length > 0 && <React.Fragment>
+                    <span className='break-pinned-list'>pinned</span>
+                    <div className='pinned-note-list'>
+                        {pinnedNotes.map((note) => (
+                        <NotePreview note={note} key={note.id} onRemoveNote={onRemoveNote} onArchiveNote={onArchiveNote} onNoteClick={onNoteClick} onPinNote={onPinNote} className='note'/>
+                        ))}
+                    </div>
+                    <span className='break-othres-list'>others</span>
+                    <div className='others-note-list'>
+                        {notes.map((note) => (
+                        !note.isPinned && 
+                        <NotePreview note={note} key={note.id} onRemoveNote={onRemoveNote} onArchiveNote={onArchiveNote} onNoteClick={onNoteClick} onPinNote={onPinNote} className='note'/>
+                        ))}
+                    </div>
+                    </React.Fragment>
                 }
                 
                 {/* <div> */}
